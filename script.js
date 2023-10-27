bff1302e849b915fcb8b0eb0854d488d4ea8c681
Kundan Patil <kundanpatil932577@gmail.com>
2023-10-27 19:18:48 +0530

    added bootstrap

--- script.js
+++ script.js
@@ -2,19 +2,53 @@
 // let b = prompt("Enter Color");
 // document.body.style.backgroundColor = b;
 
-const handleClick = () =>{
+const handleClick = () => {
     let c = document.getElementById("ip").value;
     document.body.style.backgroundColor = c;
 }
 
-const calculate = () =>{
-    
+
+const add = () => {
     let c = document.getElementById("ip1").value;
     c = Number(c);
 
     let d = document.getElementById("ip2").value;
     d = Number(d);
-    let x = document.getElementById("add");
-    x.innerHTML = c+d;
-    
+
+    let x = document.getElementById("cal");
+    x.innerHTML = c + d;
+
+}
+const sub = () => {
+    let c = document.getElementById("ip1").value;
+    c = Number(c);
+
+    let d = document.getElementById("ip2").value;
+    d = Number(d);
+
+    let x = document.getElementById("cal");
+    x.innerHTML = c - d;
+
+}
+const mul = () => {
+    let c = document.getElementById("ip1").value;
+    c = Number(c);
+
+    let d = document.getElementById("ip2").value;
+    d = Number(d);
+
+    let x = document.getElementById("cal");
+    x.innerHTML = c * d;
+
+}
+const div = () => {
+    let c = document.getElementById("ip1").value;
+    c = Number(c);
+
+    let d = document.getElementById("ip2").value;
+    d = Number(d);
+
+    let x = document.getElementById("cal");
+    x.innerHTML = c / d;
+
 }
